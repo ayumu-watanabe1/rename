1ab52caf10d970574fc524ae7ef0e991b94abb69
stuayu <60843145+stuayu@users.noreply.github.com>
2021-05-15 16:35:46 +0900

    add DatasetRenew from Python-test/json-test/tst_b.py

---
+++ develop-src/DatasetRenew.py
@@ -0,0 +1,53 @@
+"""
+このソースコードはFukuda-B[https://github.com/stuayu/Python-test/blob/main/json-test/test_b.py]
+によって書かれたものを移植して利用しています。
+Bさんありがとう
+"""
+
+'''
+    年ごとに保存するびぃ
+'''
+
+
+import os
+import json
+import requests
+url = "http://cal.syoboi.jp/json.php?Req=TitleLarge"
+save_dir = "/dataset/"
+
+
+def tvList_get(url):
+    ''' Get tv program list '''
+    session = requests.Session()
+    data = session.get(url)
+    json_data = json.loads(data.text)
+
+    map_dict = {}
+
+    for i in range(1, len(json_data['Titles'])):
+        year = str(json_data['Titles'][str(i)]['FirstYear'])
+        tit = json_data['Titles'][str(i)]['Title']
+
+        if year in map_dict:
+            map_arr = list(map_dict[year])
+        else:
+            map_arr = []
+
+        map_arr.append(tit)
+        map_dict[year] = map_arr
+    return map_dict
+
+
+def tvList_save(tv_l):
+    ''' Save tv program list '''
+    print(os.getcwd())
+    os.chdir(os.getcwd()+save_dir)
+    for i in tv_l.keys():
+        fp = open(i+'.txt', 'w', encoding='UTF-8')
+        fp.write('\n'.join(tv_l[i]))
+        fp.close
+
+
+if __name__ == '__main__':
+    tv_l = tvList_get(url)
+    tvList_save(tv_l)
